067a0b00cd38a9f790f4b5f6524a54590761f9b4
RussianSenpa1 <milkhailmelnikov@gmail.com>
2022-03-15 20:46:36 +0300

    Добавлены локаторы формы логина и регистрации, добавлен файл login_page.py c тестами логин формы,формы регистрации и Url логин страницы

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,7 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_SELECTOR = (By.ID, "login_form")
+    REGISTER_SELECTOR = (By.ID, "register_form")
