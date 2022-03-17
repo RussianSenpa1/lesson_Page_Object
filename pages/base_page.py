94105e125d33d9ba3b1c064d7b1a2e982a184422
RussianSenpa1 <milkhailmelnikov@gmail.com>
2022-03-17 18:31:21 +0300

    Добавлены комментарии. Разбиты тесты страницы логина

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,12 +6,12 @@ class BasePage():
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def is_element_present(self, how, what):
+    def is_element_present(self, how, what):        #Метод проверки элемента на странице
         try:
             self.browser.find_element(how, what)
         except (NoSuchElementException):
             return False
         return True
 
-    def open(self):
+    def open(self):                    #Метод открытия страницы
         self.browser.get(self.url)
